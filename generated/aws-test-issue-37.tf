0c2dcbc17f689ba128075a4e99b283bc8ff048be
jesxav2005-coder <jesxav2005@gmail.com>
2026-06-11 08:29:20 +0530

    fix: remove duplicate and conflicting test terraform files from generated folder

--- generated/aws-test-issue-37.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "web_server_test" {
-  ami           = "ami-123456"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name        = "ProvisionedServer"
-    Environment = "test"
-  }
-}
